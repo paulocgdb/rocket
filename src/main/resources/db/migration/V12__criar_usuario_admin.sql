76959fb48ba9036e83693268784a2692c26c87a9
paulocgdb <paulocgdb@gmail.com>
2023-03-06 11:17:37 -0300

    feature: Atualização e-mail admin

--- src/main/resources/db/migration/V12__criar_usuario_admin.sql
+++ src/main/resources/db/migration/V12__criar_usuario_admin.sql
@@ -1,2 +1,2 @@
 INSERT INTO usuario (id, nome, data_nascimento, cpf, nome_da_mae, email, senha, cidade_id, perfil_id)
-VALUES (1, 'admin', '2023-03-02 03:00:00.000000', '11111111111', 'asasas', 'admin@gmail.com', 'admin', 9, 1);
+VALUES (1, 'admin', '2023-03-02 03:00:00.000000', '11111111111', 'asasas', 'admin@rocket.com', 'admin', 9, 1);
